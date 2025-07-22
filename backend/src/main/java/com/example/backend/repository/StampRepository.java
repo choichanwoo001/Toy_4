104514a88514510b75a8263db01135389308de9f
최찬우 <114642968+choichanwoo001@users.noreply.github.com>
2025-07-22 17:45:05 +0900

    Revert "feat: 일기(다이어리) 서비스의 핵심 기능 및 API를 구현"
    This reverts commit 43e9833752dfce0be00a852d2ea31be047184e8a.

--- backend/src/main/java/com/example/backend/repository/StampRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Stamp;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StampRepository extends JpaRepository<Stamp, Long> {
-} 
