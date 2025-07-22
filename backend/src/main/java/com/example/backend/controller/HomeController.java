4a3173ce51f6c2e6f766c27d3aac23ba2fba13ca
장희성 <rhddbr24@gmail.com>
2025-07-22 21:12:12 +0900

    type : refactor -HomeController 불필요한 import 제거 -application.properties session 설정 추가

--- backend/src/main/java/com/example/backend/controller/HomeController.java
+++ backend/src/main/java/com/example/backend/controller/HomeController.java
@@ -1,15 +1,8 @@
 package com.example.backend.controller;
 
-import com.example.backend.entity.TestEntity;
-import com.example.backend.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -21,4 +14,5 @@ public class HomeController {
         return "layout/base";
     }
 
+    
 }
