f8f1bd27cd5eafefafe0781dd0ee530ae8438f5a
yourkey94 <yulki000@naver.com>
2025-07-23 18:40:17 +0900

    Revert "feat: 포인트샵(상점/구매/적용) 기능 및 API 연동 구현"
    This reverts commit 9786e8daa3d086fe9dc456ae9483f68e9925ab72.

--- backend/src/main/java/com/example/backend/repository/UserStampHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.UserStampHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserStampHistoryRepository extends JpaRepository<UserStampHistory, Long> {
-} 
--- backend/src/main/java/com/example/backend/repository/UserStampRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.UserStamp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserStampRepository extends JpaRepository<UserStamp, Long> {
-} 
